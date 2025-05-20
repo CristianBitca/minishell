2284044787afbd51f7260543afcc8e5c426a9b98
Cristian Bitca <cbitca@c2r1s2.42london.com>
2025-05-20 17:10:38 +0100

    start

---
+++ Makefile
@@ -0,0 +1,43 @@
+CC      = cc
+# # CFLAGS  = -Wall -Wextra -Werror 
+# CFLAGS  = -Wall -Wextra -Werror -fsanitize=address -g3 -O0
+
+SRC_DIR     = src
+INC_DIR     = include
+LIB_DIR     = lib
+LIBFT_DIR   = $(LIB_DIR)/libft
+
+LIBFT_A     = $(LIBFT_DIR)/libft.a
+LIBFT_LIB   = -L$(LIBFT_DIR) -lft
+
+SRCS    = $(shell find $(SRC_DIR) -name "*.c")
+OBJS    = $(SRCS:.c=.o)
+
+NAME    = minishell
+
+all: $(LIBFT_A) $(NAME)
+
+$(LIBFT_A):
+	@echo "Compiling libft..."
+	$(MAKE) -sC $(LIBFT_DIR)
+
+$(NAME): $(OBJS)
+	@echo "Compiling $(NAME)..."
+	$(CC) $(CFLAGS) $(OBJS) -I$(INC_DIR) -I$(LIBFT_DIR) $(LIBFT_LIB) -o $(NAME)
+
+clean:
+	@echo "Cleaning object files..."
+	rm -f $(OBJS)
+	$(MAKE) -sC $(LIBFT_DIR) clean
+
+fclean: clean
+	@echo "Cleaning executable and libft.a..."
+	rm -f $(NAME)
+	$(MAKE) -sC $(LIBFT_DIR) fclean
+
+re: fclean all
+
+run: all
+	./$(NAME)
+
+.PHONY: all clean fclean re run
---
+++ include/minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbitca <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/18 18:48:25 by cbitca            #+#    #+#             */
+/*   Updated: 2025/05/18 18:48:29 by cbitca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <errno.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <signal.h>
+# include <termios.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "../lib/libft/include/libft.h"
+# include "src/env/env.h"
+
+#endif
