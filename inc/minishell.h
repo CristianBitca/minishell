148885c5118b9695349088080cb3747ea1beb0af
Sean Kirwan <skirwan@c1r4s4.42london.com>
2025-06-18 14:41:44 +0100

    Created basic prompt with basic rl loop

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,34 +6,39 @@
 /*   By: cbitca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 18:48:25 by cbitca            #+#    #+#             */
-/*   Updated: 2025/06/11 12:00:43 by skirwan          ###   ########.fr       */
+/*   Updated: 2025/06/18 11:56:50 by skirwan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <errno.h>
 # include <sys/wait.h>
 # include <sys/types.h>
-# include <signal.h>
 # include <termios.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-# include "../inc/libft.h"
-# include "../src/env/env.h"
+# include "libft.h"
+
+typedef struct s_env_var
+{
+	char				*key;
+	char				*value;
+	struct s_env_var	*next;
+}	t_env_var;
 
 typedef struct s_data
 {
 	t_env_var	*env;
+	t_list		*cmd_list;
+	int			exit_status;
 }	t_data;
 
-typedef struct s_cmd_data
+typedef struct s_cmd
 {
 	char	**argv;
-	char	**envp;
 	int	infilefd;
 	int	outfilefd;
-} t_cmd_data;
+} t_cmd;
 
 #endif
