93b155c468c0d31cdcbf4b7fb076737e606fbcd9
Sean Kirwan <skirwan@student.42london.com>
2025-08-04 10:11:17 +0100

    Added fd redirections i/o to execute_in_child in child_process.c, also changed same function to return an int rather than void. Therefore wstatus in waitpid can pick up if an error is returned

--- inc/execution.h
+++ inc/execution.h
@@ -6,7 +6,7 @@
 /*   By: skirwan <skirwan@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 12:33:13 by skirwan           #+#    #+#             */
-/*   Updated: 2025/08/02 16:35:56 by skirwan          ###   ########.fr       */
+/*   Updated: 2025/08/04 10:03:36 by skirwan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		is_built_in(t_prcs *process);
 int		execute_built_in(t_data *data, t_prcs *process);
 
 // child_process.c
-void	execute_in_child(t_data *data, t_prcs *process);
+int		execute_in_child(t_data *data, t_prcs *process);
 
 // single_command.c
 void	single_cmd(t_data *data, t_prcs *process);
--- src/execution/child_process.c
+++ src/execution/child_process.c
@@ -6,29 +6,38 @@
 /*   By: skirwan <skirwan@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 16:20:51 by skirwan           #+#    #+#             */
-/*   Updated: 2025/08/02 17:44:48 by skirwan          ###   ########.fr       */
+/*   Updated: 2025/08/04 10:04:25 by skirwan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 #include "built_in.h"
+#include <unistd.h>
 
 // On success execve does not return, so we must get the exit_status of the last
 // command in the waitpid command rather than through the return value.
 // If execve fails or if the cmd to pass is NULL, we must free all allocated memory
 // and exit, because we are in the child process which copies all memory at the time
 // of forking.
-void	execute_in_child(t_data *data, t_prcs *process)
+int	execute_in_child(t_data *data, t_prcs *process)
 {
 	int		execve_status;
 	char	**envp;
 
 	envp = make_envp(data);
+	if (dup2(process->outfilefd, STDOUT_FILENO) < 0
+		|| dup2(process->infilefd, STDIN_FILENO < 0))
+	{
+		perror(NULL);
+		free_envp(envp);
+		// free all mallocs and exit
+		return (1);
+	}
 	if (process->argv[0] == NULL)
 	{
 		free_envp(envp);
 		// free all mallocs and exit;
-		return ;
+		return (1);
 	}
 	execve_status = execve(process->argv[0], process->argv, envp);
 	if (execve_status < 0)
@@ -36,5 +45,5 @@ void	execute_in_child(t_data *data, t_prcs *process)
 		free_envp(envp);
 		// free all mallocs and exit;
 	}
-	return ;
+	return (1);
 }
